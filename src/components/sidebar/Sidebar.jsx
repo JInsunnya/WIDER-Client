7c3b88c5d017382bf94db895c9a32ed76179a578
JInsunnya <jsk5754@naver.com>
2025-05-03 18:48:49 +0900

    feat: 데스크탑용 홈페이지 레이아웃, 헤더 및 사이드바 컴포넌트 구현

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import * as S from './SidebarStyles.jsx';
+import ChatOn from '../../assets/ChatOn.svg';
+import ChatOff from '../../assets/ChatOff.svg';
+import HomeOn from '../../assets/HomeDeskOn.svg';
+import HomeOff from '../../assets/HomeDeskOff.svg';
+import RecordOn from '../../assets/RecordDeskOn.svg';
+import RecordOff from '../../assets/RecordDeskOff.svg';
+import InsightOn from '../../assets/InsightDeskOn.svg';
+import InsightOff from '../../assets/InsightDeskOff.svg';
+import SettingOn from '../../assets/SettingDeskOn.svg';
+import SettingOff from '../../assets/SettingDeskOff.svg';
+
+const Sidebar = () => {
+    const location = useLocation();
+
+    const navigate = useNavigate();
+
+    const goToChat = () => {
+        navigate('/chatdesk');
+    };
+
+    const goToHome = () => {
+        navigate('/home');
+    };
+
+    const goToRecord = () => {
+        navigate('/recordsdesk');
+    };
+
+    const goToInsight = () => {
+        navigate('/insightdesk');
+    };
+
+    const goToSetting = () => {
+        navigate('/settingdesk');
+    };
+
+    return (
+        <S.Container>
+            <S.TopNav>
+                <S.Chat onClick={goToChat}>
+                    <img src={location.pathname === '/chatdesk' ? ChatOn : ChatOff} />
+                </S.Chat>
+                <S.Home onClick={goToHome}>
+                    <img src={location.pathname === '/home' ? HomeOn : HomeOff} />
+                </S.Home>
+                <S.Record onClick={goToRecord}>
+                    <img src={location.pathname === '/recordsdesk' ? RecordOn : RecordOff} />
+                </S.Record>
+                <S.Insight onClick={goToInsight}>
+                    <img src={location.pathname === '/insightdesk' ? InsightOn : InsightOff} />
+                </S.Insight>
+            </S.TopNav>
+            <S.BottomNav>
+                <S.Setting onClick={goToSetting}>
+                    <img src={location.pathname === '/setting' ? SettingOn : SettingOff} />
+                </S.Setting>
+            </S.BottomNav>
+        </S.Container>
+    );
+};
+export default Sidebar;
---
+++ src/components/sidebar/SidebarStyles.jsx
@@ -0,0 +1,67 @@
+import { styled } from 'styled-components';
+
+export const Container = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 130px;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    justify-content: center;
+    background-color: #f9f9f9;
+    z-index: 10;
+`;
+
+export const TopNav = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    margin-top: -10px;
+`;
+
+export const BottomNav = styled.div`
+    margin-top: 280px;
+`;
+
+export const Chat = styled.div`
+    img {
+        width: 85px;
+        height: 85px;
+    }
+    cursor: pointer;
+`;
+
+export const Home = styled.div`
+    img {
+        width: 85px;
+        height: 85px;
+    }
+    cursor: pointer;
+`;
+
+export const Record = styled.div`
+    img {
+        width: 85px;
+        height: 85px;
+    }
+    cursor: pointer;
+`;
+
+export const Insight = styled.div`
+    img {
+        width: 85px;
+        height: 85px;
+    }
+    cursor: pointer;
+`;
+
+export const Setting = styled.div`
+    img {
+        width: 85px;
+        height: 85px;
+    }
+    cursor: pointer;
+`;
