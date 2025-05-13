d57f2bd3fa873d0fb7ed19fd8041f08258f15b64
JInsunnya <jsk5754@naver.com>
2025-05-13 14:03:49 +0900

    feat: 데스크탑 헤더에 /chat 경로일 때만 Report 버튼 표시 및 이동 기능 추가

---
+++ src/components/header/HomeDeskHeaderStyles.jsx
@@ -0,0 +1,51 @@
+import { styled } from 'styled-components';
+
+export const Container = styled.div`
+    position: fixed;
+    top: 0;
+    left: 130px;
+    width: calc(100% - 130px);
+    height: 100px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    text-align: center;
+    justify-content: flex-start;
+    background-color: #d8eaff;
+    z-index: 9;
+`;
+
+export const Logo = styled.div`
+    img {
+        width: 80px;
+        height: 70px;
+    }
+    margin-right: 30px;
+    margin-left: 50px;
+`;
+
+export const Body = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 10px;
+`;
+
+export const Title = styled.div`
+    font-size: 28px;
+    font-weight: bold;
+`;
+
+export const Text = styled.div`
+    font-size: 14px;
+    font-weight: bold;
+`;
+
+export const Report = styled.div`
+    img {
+        width: 70px;
+        height: 70px;
+    }
+    margin-left: auto;
+    margin-right: 50px;
+`;
