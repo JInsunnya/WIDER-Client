6f9b5165698793eaf048cd7cf3321de3c4785cfd
JInsunnya <jsk5754@naver.com>
2025-05-24 19:54:10 +0900

    feat: 헤더 이미지 및 스타일 수정, SettingsHeader 추가 및 조건부 렌더링 적용

--- src/components/header/HomeDeskHeaderStyles.jsx
+++ src/components/header/HomeDeskHeaderStyles.jsx
@@ -11,34 +11,51 @@ export const Container = styled.div`
     align-items: center;
     text-align: center;
     justify-content: flex-start;
-    background-color: #d8eaff;
+    background-color: #91c0f2;
     z-index: 9;
 `;
 
 export const Logo = styled.div`
     img {
-        width: 80px;
-        height: 70px;
+        width: 90px;
+        height: 90px;
     }
-    margin-right: 30px;
     margin-left: 50px;
 `;
 
 export const Body = styled.div`
+    position: relative;
+    width: 320px;
+    height: 80px;
     display: flex;
     flex-direction: column;
-    align-items: flex-start;
-    gap: 10px;
+    align-items: center;
+    justify-content: center;
+    gap: 5px;
+    color: #4e4e4e;
+    font-size: 18px;
+    padding: 15px 30px;
+    font-weight: bold;
+`;
+
+export const BackgroundImg = styled.img`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 300px;
+    height: 80px;
+    object-fit: cover;
+    z-index: 0;
 `;
 
 export const Title = styled.div`
-    font-size: 28px;
-    font-weight: bold;
+    position: relative;
+    z-index: 1;
 `;
 
 export const Text = styled.div`
-    font-size: 14px;
-    font-weight: bold;
+    position: relative;
+    z-index: 1;
 `;
 
 export const Report = styled.div`
@@ -48,4 +65,14 @@ export const Report = styled.div`
     }
     margin-left: auto;
     margin-right: 50px;
+    cursor: pointer;
+`;
+
+export const TextRecord = styled.div`
+    font-weight: bold;
+    font-size: 18px;
+    color: #4e4e4e;
+    z-index: 1;
+    position: relative;
+    text-align: center;
 `;
--- src/pages/chat/ChatDeskScreenStyles.jsx
+++ src/pages/chat/ChatDeskScreenStyles.jsx
@@ -6,75 +6,76 @@ export const Container = styled.div`
     position: relative;
     margin: 0 auto;
     padding: 0;
-    padding-top: 100px;
     box-sizing: border-box;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    text-align: center;
-    margin-top: 100px;
+    margin-top: 70px;
+    margin-left: 130px;
 `;
 
 export const Content = styled.div`
     margin-top: 5%;
+    padding-bottom: 100px;
+    padding-top: 20px;
 `;
 
 export const Date = styled.div`
-    position: fixed;
-    top: 110px;
-    width: 393px;
-    left: 50%;
     width: fit-content;
-    display: flex;
-    justify-content: center;
-    text-align: center;
+    width: fit-content;
     font-size: 16px;
     font-weight: bold;
-    padding: 8px 16px;
+    padding: 0px 16px;
     z-index: 1;
+    margin: 0 auto;
+    margin-top: -1%;
+    margin-bottom: 3%;
 `;
 
 export const Chat = styled.div`
     display: flex;
     flex-direction: column;
-    gap: 30px;
-    padding: 0 20px;
 `;
 
 export const Chatbot = styled.div`
-    align-self: flex-start;
     background-color: #f5f5f5;
-    padding: 20px 26px;
+    padding: 20px 20px;
     border-radius: 20px;
     font-size: 16px;
-    text-align: center;
+    text-align: left;
     width: fit-content;
-    width: 70%;
     line-height: 1.6;
-    max-width: 60%;
     min-height: 50px;
-    margin-left: -20px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    max-width: 55%;
+    margin-right: auto;
+    margin-left: 30px;
 `;
 
 export const ChatUser = styled.div`
-    align-self: flex-end;
     background-color: #d8f2ff;
-    padding: 20px 26px;
+    padding: 20px 20px;
     border-radius: 20px;
     font-size: 16px;
-    text-align: center;
+    text-align: left;
     width: fit-content;
     line-height: 1.6;
-    max-width: 70%;
+    max-width: 55%;
     min-height: 50px;
-    margin-right: -20px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    margin-left: auto;
 `;
 
 export const InputBox = styled.div`
     position: fixed;
     bottom: 0;
+    left: 130px;
     width: 100%;
+    width: calc(100% - 130px);
+    max-width: 1024px;
     display: flex;
     align-items: center;
     justify-content: center;
@@ -82,10 +83,20 @@ export const InputBox = styled.div`
     z-index: 2;
 `;
 
+export const InputBackground = styled.div`
+    position: fixed;
+    bottom: 0;
+    left: 130px;
+    width: calc(100% - 130px);
+    height: 90px;
+    background-color: white;
+    z-index: 1;
+`;
+
 export const InputWrapper = styled.div`
     position: relative;
     width: 100%;
-    padding: 0 60px;
+    padding: 0 20px;
     max-width: 80%;
 `;
 
@@ -93,7 +104,7 @@ export const Input = styled.input`
     width: 100%;
     padding: 0px 45px 0px 20px;
     border-radius: 20px;
-    border: 1px solid #000000;
+    border: 1px solid #6ba9ec;
     font-size: 16px;
     outline: none;
     height: 45px;
@@ -103,7 +114,7 @@ export const Input = styled.input`
 export const Send = styled.div`
     position: absolute;
     top: 50%;
-    right: 75px;
+    right: 35px;
     transform: translateY(-50%);
     cursor: pointer;
     img {
