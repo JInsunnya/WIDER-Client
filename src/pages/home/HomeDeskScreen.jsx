2ccd7fda05f4a87205f45e8a04cd83cdfe89f7e3
JInsunnya <jsk5754@naver.com>
2025-06-09 05:46:40 +0000

    feat: 오늘의 질문 API 연동 및 홈 화면 표시 로직 구현

--- src/pages/home/HomeDeskScreen.jsx
+++ src/pages/home/HomeDeskScreen.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { getTodayTopicApi } from '../../api/home/getTopicApi';
 import * as Hd from './HomeDeskScreenStyles.jsx';
 import HomeDeskHeader from '../../components/header/HomeDeskHeader';
 import Sidebar from '../../components/sidebar/Sidebar';
 
 const HomeDesk = () => {
     const navigate = useNavigate();
+    const token = useSelector((state) => state.user.token);
+    const [todayTopic, setTodayTopic] = useState('');
 
     const goToChat = () => {
         navigate('/chat');
@@ -15,12 +19,26 @@ const HomeDesk = () => {
         navigate('/report');
     };
 
+    useEffect(() => {
+        const fetchTopic = async () => {
+            try {
+                const res = await getTodayTopicApi(token);
+                setTodayTopic(res || '오늘의 질문을 불러오지 못했습니다.');
+            } catch (e) {
+                setTodayTopic('질문을 불러오는 데 실패했습니다.');
+                console.error(e);
+            }
+        };
+
+        fetchTopic();
+    }, [token]);
+
     return (
         <Hd.Container>
             <HomeDeskHeader />
             <Hd.Content>
                 <Hd.Title>오늘의 질문</Hd.Title>
-                <Hd.Question onClick={goToChat}>대선 후보 단일화의 주요 후보는 누구인가요?</Hd.Question>
+                <Hd.Question onClick={goToChat}>{todayTopic}</Hd.Question>
                 <Hd.StartConversation>터치하여 대화를 시작하세요!</Hd.StartConversation>
                 <Hd.ReportCreated>어제의 리포트가 생성되었습니다!</Hd.ReportCreated>
                 <Hd.ButtonReport onClick={goToReport}>리포트 보러 가기</Hd.ButtonReport>
